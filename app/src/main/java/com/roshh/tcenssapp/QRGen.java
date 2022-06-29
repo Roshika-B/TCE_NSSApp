0094626075b5e249aedc361b85e76b841cf78fb7
Roshika-B <roshikabs1@gmail.com>
2022-06-29 22:16:05 +0530

    git --commit

--- app/src/main/java/com/roshh/tcenssapp/QRGen.java
+++ app/src/main/java/com/roshh/tcenssapp/QRGen.java
@@ -31,6 +31,7 @@ public class QRGen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrgen);
+        getSupportActionBar().hide();
 
         // initializing all variables.
         qrCodeIV = findViewById(R.id.idIVQrcode);
